a812a3422a9d6b5d86fc91e02c2cabf27ec19730
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-13 00:48:40 +0000

    install of gem and nokogiri gems

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,12 @@
 class CLI 
   
     def run 
-      puts "Hello World"
+      self.greeting
+      Scraper.scrape_bars
     end 
   
+    def greeting 
+      puts "\nAloha! Welcome to the World's 15 Most Important Tiki Bars!\n\n"
+    end
   
 end 
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,15 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 require_relative "./tiki_bars/version"
+require_relative './cli.rb'
+require_relative './scraper.rb'
+
+
+
 
 module TikiBars
   class Error < StandardError; end
   # Your code goes here...
 end
-
-require_relative './cli.rb'
