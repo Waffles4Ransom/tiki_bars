bbcb42676e4c4c667b7e9c7d4e1e65cf0e89efcb
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-17 00:40:12 +0000

    removing launchy for now

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,16 +1,14 @@
 require 'pry'
 require 'nokogiri'
 require 'colorize'
-require 'launchy'
+#require 'launchy'
 require 'open-uri'
 
-LAUNCHY_DEBUG=true
 
 require_relative './tiki_bars/version.rb'
 require_relative './cli.rb'
 require_relative './scraper.rb'
 require_relative './bar.rb'
-
 
 
 module TikiBars
--- tiki_bars.gemspec
+++ tiki_bars.gemspec
@@ -42,5 +42,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "nokogiri"
   spec.add_dependency "colorize"
-  spec.add_dependency "launchy"
+  #spec.add_dependency "launchy"
 end
